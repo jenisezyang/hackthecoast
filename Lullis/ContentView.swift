0c828500d6c60ad8723a8feb22993db53407b3b0
jenisezyang <jenisezyang@gmail.com>
2026-02-07 14:01:48 -0800

    Initial Commit

---
+++ Lullis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lullis
+//
+//  Created by Jenise Yang on 2026-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lullis/LullisApp.swift
@@ -0,0 +1,17 @@
+//
+//  LullisApp.swift
+//  Lullis
+//
+//  Created by Jenise Yang on 2026-02-07.
+//
+
+import SwiftUI
+
+@main
+struct LullisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
